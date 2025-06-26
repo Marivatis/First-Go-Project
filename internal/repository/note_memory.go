1ebb878c8e0975e9facd6c057db28d72a91adb5a
Marivatis <cyvax2@gmail.com>
2025-06-26 16:06:29 +0300

    feat(repository): implement full CRUD for notes

--- internal/repository/note_memory.go
+++ internal/repository/note_memory.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 )
 
+var ErrNoteNotFound = errors.New("note not found")
+
 type NoteMemory struct {
 	mu     sync.RWMutex
 	data   map[int]entity.Note
@@ -23,9 +25,10 @@ func (r *NoteMemory) Create(note entity.Note) (int, error) {
 	defer r.mu.Unlock()
 
 	r.lastId++
-	note.Id = r.lastId
-	r.data[note.Id] = note
-	return note.Id, nil
+	n := entity.NewNote(r.lastId, note.Title, note.Body)
+
+	r.data[n.Id()] = n
+	return n.Id(), nil
 }
 
 func (r *NoteMemory) GetById(id int) (entity.Note, error) {
@@ -34,7 +37,42 @@ func (r *NoteMemory) GetById(id int) (entity.Note, error) {
 
 	note, ok := r.data[id]
 	if !ok {
-		return entity.Note{}, errors.New("note not found")
+		return entity.Note{}, ErrNoteNotFound
 	}
 	return note, nil
+}
+
+func (r *NoteMemory) GetAll() ([]entity.Note, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	notes := make([]entity.Note, 0, len(r.data))
+	for _, note := range r.data {
+		notes = append(notes, note)
+	}
+	return notes, nil
+}
+
+func (r *NoteMemory) Update(note entity.Note) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, ok := r.data[note.Id()]
+	if !ok {
+		return ErrNoteNotFound
+	}
+	r.data[note.Id()] = note
+	return nil
+}
+
+func (r *NoteMemory) Delete(id int) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, ok := r.data[id]
+	if !ok {
+		return ErrNoteNotFound
+	}
+	delete(r.data, id)
+	return nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,9 +5,9 @@ import "First-Go-Project/internal/entity"
 type NoteItem interface {
 	Create(note entity.Note) (int, error)
 	GetById(id int) (entity.Note, error)
-	//GetAll() ([]entity.Note, error)
-	//Update(note entity.Note) error
-	//Delete(id int) error
+	GetAll() ([]entity.Note, error)
+	Update(note entity.Note) error
+	Delete(id int) error
 }
 
 type Repository struct {
