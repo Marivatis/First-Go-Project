8032eef1cf3714769aca0a815e71f6afca3f65e9
Marivatis <cyvax2@gmail.com>
2025-06-26 16:04:38 +0300

    feat(entity): add readonly id with getter, constant and sorting method

--- internal/entity/note.go
+++ internal/entity/note.go
@@ -1,7 +1,29 @@
 package entity
 
+import "sort"
+
+const UnsetNoteId = -1
+
 type Note struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
+	id    int
+	Title string
+	Body  string
+}
+
+func NewNote(id int, title, body string) Note {
+	return Note{
+		id:    id,
+		Title: title,
+		Body:  body,
+	}
+}
+
+func (n *Note) Id() int {
+	return n.id
+}
+
+func SortNotesById(notes []Note) {
+	sort.Slice(notes, func(i, j int) bool {
+		return notes[i].Id() < notes[j].Id()
+	})
 }
